e597b6aed0589023b026909af9da58e999524317
migusmp <mmp2004sp@gmail.com>
2025-01-16 18:37:50 +0000

    tmux configuration updated

--- init.lua
+++ init.lua
@@ -203,6 +203,75 @@ require('lazy').setup({
             })
         end
     },
+    {
+        "aserowy/tmux.nvim",
+        config = function()
+            return require("tmux").setup(
+                {
+                    copy_sync = {
+                        -- enables copy sync. by default, all registers are synchronized.
+                        -- to control which registers are synced, see the `sync_*` options.
+                        enable = true,
+
+                        -- ignore specific tmux buffers e.g. buffer0 = true to ignore the
+                        -- first buffer or named_buffer_name = true to ignore a named tmux
+                        -- buffer with name named_buffer_name :)
+                        ignore_buffers = { empty = false },
+
+                        -- TMUX >= 3.2: all yanks (and deletes) will get redirected to system
+                        -- clipboard by tmux
+                        redirect_to_clipboard = false,
+
+                        -- offset controls where register sync starts
+                        -- e.g. offset 2 lets registers 0 and 1 untouched
+                        register_offset = 0,
+
+                        -- overwrites vim.g.clipboard to redirect * and + to the system
+                        -- clipboard using tmux. If you sync your system clipboard without tmux,
+                        -- disable this option!
+                        sync_clipboard = true,
+
+                        -- synchronizes registers *, +, unnamed, and 0 till 9 with tmux buffers.
+                        sync_registers = true,
+
+                        -- synchronizes registers when pressing p and P.
+                        sync_registers_keymap_put = true,
+
+                        -- synchronizes registers when pressing (C-r) and ".
+                        sync_registers_keymap_reg = true,
+
+                        -- syncs deletes with tmux clipboard as well, it is adviced to
+                        -- do so. Nvim does not allow syncing registers 0 and 1 without
+                        -- overwriting the unnamed register. Thus, ddp would not be possible.
+                        sync_deletes = true,
+
+                        -- syncs the unnamed register with the first buffer entry from tmux.
+                        sync_unnamed = true,
+                    },
+                    navigation = {
+                        -- cycles to opposite pane while navigating into the border
+                        cycle_navigation = true,
+
+                        -- enables default keybindings (C-hjkl) for normal mode
+                        enable_default_keybindings = true,
+
+                        -- prevents unzoom tmux when navigating beyond vim border
+                        persist_zoom = false,
+                    },
+                    resize = {
+                        -- enables default keybindings (A-hjkl) for normal mode
+                        enable_default_keybindings = true,
+
+                        -- sets resize steps for x axis
+                        resize_step_x = 1,
+
+                        -- sets resize steps for y axis
+                        resize_step_y = 1,
+                    }
+                }
+            )
+        end
+    },
 })
 
 -- Configuración de LSP y cmp
@@ -298,13 +367,16 @@ require("mason-lspconfig").setup({
                             preferred = "fernflower", -- Establecer el descompilador preferido
                         },
                         build = {
-                            autoBuild = false, -- Deshabilitar la construcción automática del proyecto
-                            autoRefresh = false, -- Evitar refrescar constantemente el proyecto
+                            autoBuild = true,   -- Deshabilitar la construcción automática del proyecto
+                            autoRefresh = true, -- Evitar refrescar constantemente el proyecto
                         },
                         errors = {
-                            enabled = true,     -- Asegurar que los errores se linteren
-                            validation = "interactive", -- Puede hacer que se validen solo cuando se le indica
+                            enabled = true, -- Asegurar que los errores se linteren
+                            -- validation = "interactive", -- Puede hacer que se validen solo cuando se le indica
                         },
+                        signatureHelp = { enabled = true },
+                        import = { enabled = true },
+                        rename = { enabled = true },
                     },
                 },
                 init_options = {
@@ -313,7 +385,7 @@ require("mason-lspconfig").setup({
                 root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", "pom.xml", "build.gradle" },
                     { upward = true })[1]),
                 cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" }, -- Asegúrate de tener la ruta correcta a jdtls
-                on_attach = on_attach,                          -- Aquí debes definir la función `on_attach` si no lo has hecho
+                on_attach = on_attach,                                  -- Aquí debes definir la función `on_attach` si no lo has hecho
             })
         end,
     }
